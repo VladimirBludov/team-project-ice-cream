800f42be96a5f20676d3b61a4a0bd4c2c0973749
Mozgarik <91159717+Mozgarik@users.noreply.github.com>
2021-12-03 17:49:27 +0200

    update

---
+++ src/modal.js
@@ -0,0 +1,30 @@
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector('[data-modal-open]'),
+        closeModalBtn: document.querySelector('[data-modal-close]'),
+        modalAbout: document.querySelector('[data-modal]'),
+    };
+
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+
+    function toggleModal() {
+        refs.modalAbout.classList.toggle('visually-hidden');
+    }
+})();
+
+
+(() => {
+     const refs = {
+         openModalBtn: document.querySelector('[data-modal-open]'),
+         closeModalBtn: document.querySelector('[data-modal-close]'),
+         modalAbout: document.querySelector('[modal]'),
+     };
+
+     refs.openModalBtn.addEventListener('click', toggleModal);
+     refs.closeModalBtn.addEventListener('click', toggleModal);
+
+     function toggleModal() {
+         refs.modalAbout.classList.toggle('visually-hidden');
+     }
+ })();
