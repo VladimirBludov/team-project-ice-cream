72a4af084144b1f096a7e8e0f06e004f5f1e6420
VladimirBludov <vladimir.bloodov@gmail.com>
2021-12-05 15:09:03 +0200

    fix jumping murkup

---
+++ src/js/modal.js
@@ -0,0 +1,23 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('.about__btn'),
+    closeModalBtn: document.querySelector('.read__modal-close'),
+    modalAbout: document.querySelector('.read__modal'),
+    body: document.querySelector('.body'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    const isMenuOpen = refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
+
+    refs.openModalBtn.setAttribute('aria-expanded', !isMenuOpen);
+
+    const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+
+    refs.body.classList.toggle('disable-scroll');
+    refs.modalAbout.classList.toggle('visually-hidden');
+  }
+})();
