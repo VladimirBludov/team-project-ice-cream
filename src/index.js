6c5176feb2cef283d71adadc50f8ccb57f9b1b6d
Vladimir Bludov <vladimir.bloodov@gmail.com>
2021-12-06 13:30:11 +0200

    fix murkup

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import './sass/main.scss';
 import './js/modal';
 import './js/up';
 import './js/gallery-modal.js';
+import './js/multi-modal.js';
+import './js/product-card.js';
+import './js/mobile-menu';
