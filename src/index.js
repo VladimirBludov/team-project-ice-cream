3d28c97ddcc731486eb9048586e29ca4f7181f74
PavloLevchenko <teasetrnet@gmail.com>
2021-12-08 19:00:00 +0200

    Убирает файл product-card.js. Добавляет modal-transition.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import './js/modal.js';
 import './js/up.js';
 import './js/gallery-modal.js';
 import './js/multi-modal.js';
-import './js/product-card.js';
+import './js/modal-transition.js';
 import './js/mobile-menu.js';
 import './js/scroll-to-anchor.js';
